4cadb998ee27d2a3896c0a559b11a577eb11c73f
StephenDodd1 <skdodd@live.com>
2023-01-29 13:33:52 -0600

    updated to add new note field

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const arrContainer = document.getElementById('notes-array')
+const addNoteBtn = document.getElementById('add-note-btn')
 
 let noteElements = Array.from([1,2,3],(_,i) =>{
   return(`<div id=${'note-' + i} class='note-container'>
@@ -11,44 +12,124 @@ let noteElements = Array.from([1,2,3],(_,i) =>{
     <div id=${'note-details-' + i}></div>
   </div>`)}
 )
+
 arrContainer.innerHTML = noteElements.join('')
+
 const openButtons = document.querySelectorAll('.open-btn')
 const closeButtons = document.querySelectorAll('.close-btn')
 const deleteButtons = document.querySelectorAll('.delete-btn')
+
 const currentNoteValue = 'TEST A NOTE BODY.'
+let tempNoteValue = currentNoteValue
+
 var currentNoteElement = `
-<div>
-  <div>${currentNoteValue}</div>
-  <button>edit</button>
-  <textarea rows=5 placeholder='${currentNoteValue}'></textarea>
+<div id='note-container'>
+  <div id='note-text' class=''>${tempNoteValue}</div>
+  <button id='note-edit-btn' class=''>edit</button>
+  <div>
+    <button id='note-cancel-btn' class='hidden'>cancel</button>
+  </div>
+  <div>
+    <textarea id='note-field' class='hidden' rows=5 placeholder='Update note here.'>${tempNoteValue}</textarea>
+  </div>
+  <div>
+    <button id='note-save-btn' class='hidden'>save</button>
+  </div>
 </div>`
+const toggleNotesBody = () => {
+  document.getElementById('note-text').classList.toggle('hidden')
+  document.getElementById('note-edit-btn').classList.toggle('hidden')
+  document.getElementById('note-cancel-btn').classList.toggle('hidden')
+  document.getElementById('note-field').classList.toggle('hidden')
+  document.getElementById('note-save-btn').classList.toggle('hidden')
+  console.log('clicked')
+}
+
+const openNote = (id) => {
+  for(let i = 0; i < openButtons.length;  i++){
+    const currentNote = document.getElementById('note-details-'+i)
+    if(i === id) {
+      console.log('ran1', i, id)
+      closeButtons[id].classList.remove('hidden')
+      openButtons[id].classList.add('hidden')
+      currentNote.innerHTML = currentNoteElement
+      console.log('click', openButtons[id])
+
+      const editButton = document.getElementById('note-edit-btn')
+      editButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        toggleNotesBody()
+      })
+
+      const cancelButton = document.getElementById('note-cancel-btn')
+      cancelButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        toggleNotesBody()
+        tempNoteValue = currentNoteValue
+      })
+      const saveButton = document.getElementById('note-save-btn')
+      saveButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        toggleNotesBody()
+        console.log('PUT request to update note: ', tempNoteValue)
+      })
+      const editNoteUpdate = document.getElementById('note-field')
+      editNoteUpdate.addEventListener('input', (e) => {
+        e.preventDefault();
+        tempNoteValue = e.target.value
+      })
+    } else {
+      console.log('ran2')
+      openButtons[id].classList.remove('hidden')
+      closeButtons[id].classList.add('hidden')
+      currentNote.innerHTML = ''
+    }
+  }
+}
+
 for(let i = 0; i < openButtons.length;  i++){
   (function(idx){
     openButtons[idx].addEventListener('click',function(event) {
       event.preventDefault();
-      closeButtons[idx].classList.remove('hidden')
-      const currentNote = document.getElementById('note-details-'+i)
-      currentNote.innerHTML = currentNoteElement
-      console.log('click', openButtons[idx])
+      openNote(i)
     })
     closeButtons[idx].addEventListener('click',function(event) {
       event.preventDefault();
-      openButtons[idx].classList.add('hidden')
+      openButtons[idx].classList.remove('hidden')
+      closeButtons[idx].classList.add('hidden')
       const currentNote = document.getElementById('note-details-'+i)
       console.log(currentNote.innterHTML)
       currentNote.innerHTML = ''
       console.log('click', closeButtons[idx])
     })
+    deleteButtons[i].addEventListener('click', function(event){
+      event.preventDefault();
+      const remainingElements = noteElements.filter((element, idx) => i !== idx)
+      noteElements = [...remainingElements]
+      console.log('noteElements', noteElements)
+      arrContainer.innerHTML = noteElements.join('')
+    })
     }
   )(i)
 }
 
-for(let i = 0; i < deleteButtons.length; i++){
-  deleteButtons[i].addEventListener('click', function(event){
-    event.preventDefault();
-    const remainingElements = noteElements.filter((element, idx) => i !== idx)
-    noteElements = [...remainingElements]
-    console.log('noteElements', noteElements)
-    arrContainer.innerHTML = noteElements.join('')
+addNoteBtn.addEventListener('click', (e) => {
+  console.log(e)
+  const addNoteContainer = document.getElementById('add-note-container')
+  addNoteContainer.innerHTML = currentNoteElement
+  toggleNotesBody()
+  const editNoteUpdate = document.getElementById('note-field')
+  editNoteUpdate.addEventListener('input', (e) => {
+    e.preventDefault();
+    tempNoteValue = e.target.value
   })
-}
+  const saveButton = document.getElementById('note-save-btn')
+  saveButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('POST request to update note: ', tempNoteValue)
+    const submittedNote = new Promise().resolve(() => 'success')
+    submittedNote.resolve( (status) => {
+      addNoteContainer.innerHTML = `<div>{add note - ${status}}</div>`
+    })
+  })
+})
